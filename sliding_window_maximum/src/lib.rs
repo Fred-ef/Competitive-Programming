5a5043ae6bb833566ae04e16469e0a58a9b94b50
FredFed <thenedbear@hotmail.it>
2022-10-20 03:32:21 +0200

    Implemented Sliding Windows Maximum solutions

--- sliding_window_maximum/src/lib.rs
+++ sliding_window_maximum/src/lib.rs
@@ -21,15 +21,69 @@ pub fn brute_force_idiomatic(v: &Vec<i32>, k: i32) -> Vec<i32> {
 }
 
 pub fn heap(nums: &Vec<i32>, k: i32) -> Vec<i32> {
-    todo!();
+    let k: usize = k as usize;
+    let n: usize = nums.len();
+    let mut result: Vec<i32> = Vec::with_capacity(n);
+    let mut heap: BinaryHeap<(i32, usize)> = BinaryHeap::with_capacity(n);
+    for i in 0..(n) {
+        heap.push((nums[i], i));
+        if i < (k - 1) {
+            continue;
+        }
+
+        while heap.peek().unwrap().1 < i - (k - 1) {
+            heap.pop();
+        }
+
+        result.push(heap.peek().unwrap().0);
+    }
+
+    result
 }
 
 pub fn bst(nums: &Vec<i32>, k: i32) -> Vec<i32> {
-    todo!();
+    let k: usize = k as usize;
+    let n: usize = nums.len();
+    let mut result: Vec<i32> = Vec::with_capacity(n);
+    let mut tree: BinarySearchTree<(i32, usize)> = BinarySearchTree::new();
+    for i in 0..(n) {
+        tree.insert((nums[i], i));
+        if i < (k - 1) {
+            continue;
+        }
+
+        while tree.max().unwrap().1 < i - (k - 1) {
+            tree.extract_max();
+        }
+
+        result.push(tree.max().unwrap().0);
+    }
+
+    result
 }
 
 pub fn linear(nums: &Vec<i32>, k: i32) -> Vec<i32> {
-    todo!();
+    let k: usize = k as usize;
+    let n: usize = nums.len();
+    let mut result: Vec<i32> = Vec::with_capacity(n);
+    let mut deque: VecDeque<(i32, usize)> = VecDeque::with_capacity(n);
+    for i in 0..(n) {
+        while !deque.is_empty() && deque.back().unwrap().0 <= nums[i] {
+            deque.pop_back();
+        }
+
+        deque.push_back((nums[i], i));
+
+        if i >= k - 1 {
+            while !deque.is_empty() && deque.front().unwrap().1 < i - (k - 1) {
+                deque.pop_front();
+            }
+
+            result.push(deque.front().unwrap().0);
+        }
+    }
+
+    result
 }
 
 pub fn gen_random_vector(n: usize) -> Vec<i32> {
@@ -57,7 +111,6 @@ mod tests {
         assert_eq!(results, truth);
     }
 
-    #[ignore]
     #[test]
     fn test_heap_version() {
         let k = 3;
@@ -69,7 +122,6 @@ mod tests {
         assert_eq!(results, truth);
     }
 
-    #[ignore]
     #[test]
     fn test_bst_version() {
         let k = 3;
@@ -81,7 +133,6 @@ mod tests {
         assert_eq!(results, truth);
     }
 
-    #[ignore]
     #[test]
     fn test_linear_version() {
         let k = 3;
